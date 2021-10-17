3513e2c268f5e06cc518b4cab5e81f1b2e8beba8
raaraya1 <raaraya1@miuandes.cl>
2021-10-17 09:31:11 -0300

    fix google analytics

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Colaboraty_transport_dinamic_st import Colaboraty_transport_dinamic_st
 from Facility_Location_st import Facility_Location_st
 from TSP_MTZ_st import TSP_MTZ_st
 from PIL import Image
-
+import re
 
 
 col1, col2 = st.columns([1, 5])
@@ -43,17 +43,29 @@ elif model_name == 'Facility_Location':
 elif model_name == 'TSP_MTZ':
     TSP_MTZ_st().interactive_model()
 
-# Para usar google analytics
-google_analytics_js = '''
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-210353274-1"></script>
+# agregar google analytics
+anlytcs_code = """<script async src="https://www.googletagmanager.com/gtag/js?id=UA-210353274-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-210353274-1');
-</script>
-'''
-st.components.v1.html(google_analytics_js)
-st.components.v1.iframe('https://transport-models-streamlit.herokuapp.com/google_analytics.html', height=1, scrolling=False)
+</script>"""
+
+# Fetch the path of the index.html file
+path_ind = os.path.dirname(st.__file__)+'/static/index.html'
+
+# Open the file
+with open(path_ind, 'r') as index_file:
+    data=index_file.read()
+
+    # Check whether there is GA script
+    if len(re.findall('UA-', data))==0:
+
+        # Insert Script for Google Analytics
+        with open(path_ind, 'w') as index_file_f:
+
+            # The Google Analytics script should be pasted in the header of the HTML file
+            newdata=re.sub('<head>','<head>'+anlytcs_code,data)
+
+            index_file_f.write(newdata)
