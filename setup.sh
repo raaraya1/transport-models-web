dd05083d6d6290e8e10e4c42b79b495a71be9384
raaraya1 <raaraya1@miuandes.cl>
2021-10-19 10:21:17 -0300

    fix theme

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 mkdir -p ~/.streamlit/
 
-echo "
-[theme]
-primaryColor="#4d5cea"
-backgroundColor="#000000"
-secondaryBackgroundColor="#1c1c1c"
-textColor="#ffffff"
-font="serif"
-[server]
-port = $PORT
-enableCORS = false
-headless = true
+echo "\
+[theme]\n\
+primaryColor='#4d5cea'\n\
+backgroundColor='#000000'\n\
+secondaryBackgroundColor='#1c1c1c'\n\
+textColor='#ffffff'\n\
+font='serif'\n\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
